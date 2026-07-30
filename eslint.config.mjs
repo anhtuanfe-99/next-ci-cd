e864dc5f651571e4297dbd2e579d57e2efd42d74
tuan.fe.dev <53905277+anhtuanfe-99@users.noreply.github.com>
2026-07-30 21:05:33 +0700

    fix: add no-unused-vars rule to ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,11 @@ const eslintConfig = defineConfig([
     'build/**',
     'next-env.d.ts',
   ]),
+  {
+    "rules": {
+      "no-unused-vars": "error",
+    }
+  }
 ]);
 
 export default eslintConfig;
